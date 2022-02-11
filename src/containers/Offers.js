77ceaf682b53f176b5d3950b264268dd80466a1b
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-02-11 05:58:12 +0100

    fixing some issues

--- src/containers/Offers.js
+++ src/containers/Offers.js
@@ -27,40 +27,38 @@ const Offer = () => {
   ) : (
     <section className="offers-container">
       {data.offers.map((elem) => {
-        if (elem.product_details[1]["TAILLE"])
-          return (
-            <div key={elem._id} className="offer-container">
-              <div className="user-offer">
-                <img
-                  src={elem.owner.account.avatar.secure_url}
-                  alt={`avatar de ${elem.owner.account}`}
-                />
-                <div className="offer-account-username">
-                  {elem.owner.account.username}
-                </div>
-              </div>
-              <Link to={`/offer/${elem._id}`}>
-                {" "}
-                <img
-                  className="offer-product-img"
-                  src={elem.product_pictures[0].secure_url}
-                  alt={`${elem.product_name}`}
-                />{" "}
-              </Link>
-              <div className="offer-product-infos">
-                <div className="offer-product-price">
-                  {`${Number(elem.product_price).toFixed(2).replace(".", ",")}`}{" "}
-                  €
-                </div>
-                <div className="offer-product-size">
-                  {elem.product_details[1]["TAILLE"]}
-                </div>
-                <div className="offer-product-brand">
-                  {elem.product_details[0]["MARQUE"]}
-                </div>
+        return (
+          <div key={elem._id} className="offer-container">
+            <div className="user-offer">
+              <img
+                src={elem.owner.account.avatar.secure_url}
+                alt={`avatar de ${elem.owner.account}`}
+              />
+              <div className="offer-account-username">
+                {elem.owner.account.username}
               </div>
             </div>
-          );
+            <Link to={`/offer/${elem._id}`}>
+              {" "}
+              <img
+                className="offer-product-img"
+                src={elem.product_pictures[0].secure_url}
+                alt={`${elem.product_name}`}
+              />{" "}
+            </Link>
+            <div className="offer-product-infos">
+              <div className="offer-product-price">
+                {`${Number(elem.product_price).toFixed(2).replace(".", ",")}`} €
+              </div>
+              <div className="offer-product-size">
+                {elem.product_details[1]["TAILLE"]}
+              </div>
+              <div className="offer-product-brand">
+                {elem.product_details[0]["MARQUE"]}
+              </div>
+            </div>
+          </div>
+        );
       })}
     </section>
   );
