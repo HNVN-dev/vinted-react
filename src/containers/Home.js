8265d3bf8ecd178aadd4b2b29a4535d185b09ffb
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-02-16 02:04:42 +0100

    last commit for today, bug not solved, need to check why proportions fool me in Mac Os

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,11 @@
 import Offer from "./Offers";
-/* import { Link } from "react-router-dom";
-import herobanner from "../assets/img/vinted-home-baner-wide.jpeg"; */
+import { Link } from "react-router-dom";
+import herobanner from "../assets/img/vinted-home-baner-wide.jpeg";
 
 const Home = ({ token }) => {
   return (
     <>
-      {/*  {!token && (
+      {!token && (
         <section className="hero-banner">
           <img src={herobanner} alt="" />
           <div className="hero-card">
@@ -16,7 +16,7 @@ const Home = ({ token }) => {
             <div className="discover">Découvrir comment ça marche</div>
           </div>
         </section>
-      )} */}
+      )}
 
       <Offer />
     </>
