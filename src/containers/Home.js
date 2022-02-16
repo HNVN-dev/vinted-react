9eaeb8ccb174663356b40044ed2984510b8fa531
HNVN-front <hoainamvalentinnguyen@gmail.com>
2022-02-16 01:55:07 +0100

    still can't fix netlify's proportions, problem comes with macbook, size is correct on window

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,12 +1,13 @@
 import Offer from "./Offers";
 import { Link } from "react-router-dom";
+import herobanner from "../assets/img/vinted-home-baner-wide.jpeg";
 
 const Home = ({ token }) => {
   return (
     <>
       {!token && (
         <section className="hero-banner">
-          {/* <img src={herobanner} alt="" /> */}
+          <img src={herobanner} alt="" />
           <div className="hero-card">
             <h1>Prêt à faire du tri dans vos placards ?</h1>
             <Link to="/publish">
